12767107c13bf18e0ea8f5ca242f31c9ca0cf0ed
deepranav <112204194+deepranav@users.noreply.github.com>
2022-08-26 15:47:29 -0400

    main

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
+app_url = "https://exmaple-test-ong.onrender.com"
+
+import requests
+import threading
+
+def keep_alive():
+    print("Keep Alive")
+    while True:
+        try:
+            requests.get(app_url)
+        except:
+            print("Keep Alive Error")
+        else:
+            time.sleep(60 * 5)
+threading.Thread(target=keep_alive).start()
+
+
+
 from flask import Flask
 app = Flask(__name__)
 
